db924c799b35066fa73637181f5edfccc8e010b8
Adrian Petri <adrian.petri@servustech.com>
2020-04-18 07:51:16 +0300

    hostname loadcontent

--- src/main/java/website/builder/be/api/LoadContent.java
+++ src/main/java/website/builder/be/api/LoadContent.java
@@ -8,23 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileReader;
-import java.net.URI;
+
+import static java.util.Optional.ofNullable;
 
 @RestController
 public class LoadContent {
 
     @GetMapping("/loadContent")
     public JSONObject loadContent(@RequestParam(value = "url") String url, HttpServletRequest request) {
-        System.out.println("/loadContent=" + url);
-        System.out.println("request.getRequestURL=" + URI.create(request.getRequestURL().toString()).getHost());
-        System.out.println("request.origin=" + URI.create(request.getHeader("origin")).getHost());
         try {
+            String contentPath = "content/" + getOriginHostname(request) + url + ".json";
+            System.out.println("/loadContent=" + contentPath);
             return (JSONObject) new JSONParser().parse(
                     new FileReader(getClass().getClassLoader()
-                            .getResource("content/" + url + ".json").getFile()));
+                            .getResource(contentPath)
+                            .getFile()));
         } catch (Exception e) {
             e.printStackTrace();
             return new JSONObject();
         }
+    }
+
+    private String getOriginHostname(HttpServletRequest request) {
+        return ofNullable(request.getHeader("origin"))
+                .map(url -> url.startsWith("www.") ? url.substring(4) : url)
+                .orElse("");
     }
 }
