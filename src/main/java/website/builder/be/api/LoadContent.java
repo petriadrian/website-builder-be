98c120194f6e036b19d177607a0ca9897210975e
Adrian Petri <adrian.petri@servustech.com>
2020-04-18 19:34:54 +0300

    hostname loadcontent

--- src/main/java/website/builder/be/api/LoadContent.java
+++ src/main/java/website/builder/be/api/LoadContent.java
@@ -7,40 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.FileReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static java.util.Optional.ofNullable;
+import java.util.Map;
 
 @RestController
 public class LoadContent {
 
     @GetMapping("/loadContent")
-    public ResponseEntity<?> loadContent(@RequestParam(value = "url") String url, HttpServletRequest request) {
+    public ResponseEntity<JSONObject> loadContent(@RequestParam(value = "url") String url) {
         try {
             System.out.println("/loadContent url=" + url);
-            System.out.println("/loadContent request.getRemoteHost=" + request.getRemoteHost());
-            String contentPath = "content/" + getOriginHostname(request) + url + ".json";
-            System.out.println("/loadContent fullPath=" + contentPath);
             return ResponseEntity.ok().body((JSONObject) new JSONParser().parse(
                     new FileReader(getClass().getClassLoader()
-                            .getResource(contentPath)
+                            .getResource("content/" + url + ".json")
                             .getFile())));
         } catch (Exception e) {
             e.printStackTrace();
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.badRequest().body(new JSONObject(Map.of("error", e.getMessage())));
         }
-    }
-
-    private String getOriginHostname(HttpServletRequest request) throws MalformedURLException {
-        String originUrl = new URL(getOrigin(request)).getHost();
-        return originUrl.startsWith("www.") ? originUrl.substring(4) : originUrl;
-    }
-
-    private String getOrigin(HttpServletRequest request) throws MalformedURLException {
-        return ofNullable(request.getHeader("origin"))
-                .orElseThrow(() -> new MalformedURLException("Origin request Hostname '" + request.getHeader("origin") + "' cannot be resolved"));
     }
 }
